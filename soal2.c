c55c26884d4d93b3ccf33af97fe0526f66df3dd8
AlamCahya <alamcahya3211@gmail.com>
2017-11-16 19:58:08 +0700

    same with soal1.c but with mv things

---
+++ soal2.c
@@ -0,0 +1,167 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <stdlib.h>
+
+static const char *dirpath = "/home/alamcahya/Documents";
+
+static int xmp_chmod(const char *path, mode_t mode)
+{
+	int res;
+
+	res = chmod(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+  int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+//fungsi getext dari web
+const char *getExt (const char *fspec) {
+    char *e = strrchr (fspec, '.');
+    if (e == NULL)
+        e = ""; // fast method, could also use &(fspec[strlen(fspec)]).
+    return e;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode)
+{
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+ 	int fd = 0 ;
+
+	char *ext;
+	ext= getExt(fpath);
+
+	if(strcmp(ext,".pdf") == 0 || strcmp(ext,".doc") == 0 || strcmp(ext,".txt") == 0){
+	
+		system("zenity --error --text=\"Telah terjadi kesalahan!\nFile berisi konten berbahaya\!\" --title=\"Warning\!\"");
+		
+		char newName[255];
+		char *path= "/home/alamcahya/Documents/rahasia";
+		char masuk[BUFSIZ];
+
+		strcpy(newName,fpath);
+		strcat(newName,".ditandai");
+		rename(fpath,newName);
+		DIR *cek; 
+		cek = opendir("rahasia");
+		if (cek == NULL) //cek ada tidaknya folder rahasia
+		 mkdir(path,0777);
+		
+		sprintf(masuk,sizeof(masuk),"mv %s %s",newName,path);
+		system(masuk);
+	}
+	else
+	{	
+		(void) fi;
+		fd = open(fpath, O_RDONLY);
+		if (fd == -1)
+			return -errno;
+
+		res = pread(fd, buf, size, offset);
+		if (res == -1)
+			res = -errno;
+
+		close(fd);
+		return res;
+	}
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+	int res;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+  	.read		= xmp_read,
+	.rename		= xmp_rename,
+	.mkdir		= xmp_mkdir,
+	.chmod		= xmp_chmod,
+};
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
