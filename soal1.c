32ead30efeb958d0b6cf65dcc046e9202c5d240a
AlamCahya <alamcahya3211@gmail.com>
2017-11-16 14:22:58 +0700

    trying comparing strrchr,failed

--- soal1.c
+++ soal1.c
@@ -7,109 +7,117 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <stdlib.h>
 
-static const char *dirpath = "/root/alamcahya/Downloads";
+static const char *dirpath = "/home/alamcahya/Documents";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
   int res;
-  char fpath[1000];
-  char newFile[100];
-  printf("path   : %s, len: %d\n", path, strlen(path));
-  if (strcmp(path, "/") != 0) {
-    memcpy(newFile, path, strlen(path) - 4);
-    newFile[strlen(path) - 4] = '\0';
-  } else {
-    memcpy(newFile, path, strlen(path));
-  }
-  printf("newFile: %s\n", newFile);
-  sprintf(fpath,"%s%s",dirpath, newFile);
-  res = lstat(fpath, stbuf);
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
 
-  if (res == -1)
-    return -errno;
+	if (res == -1)
+		return -errno;
 
-  return 0;
+	return 0;
 }
 
-static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
- 	      off_t offset, struct fuse_file_info *fi)
-{
- char fpath[1000];
-  if(strcmp(path,"/") == 0)
-  {
-    path=dirpath;
-    sprintf(fpath,"%s",path);
-  }
-  else sprintf(fpath, "%s%s",dirpath,path);
-  int res = 0;
-
-  DIR *dp;
-  struct dirent *de;
-
-  (void) offset;
-  (void) fi;
-
-  dp = opendir(fpath);
-  if (dp == NULL)
-    return -errno;
-
-  while ((de = readdir(dp)) != NULL) {
-    char *newName;
-    newName = strcat(de->d_name, ".bak");
-    struct stat st;
-    memset(&st, 0, sizeof(st));
-    st.st_ino = de->d_ino;
-    st.st_mode = de->d_type << 12;
-    res = (filler(buf, newName, &st, 0));
-      if(res!=0) break;
-  }
-
-  closedir(dp);
-  return 0;
-}
-
-static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
- 	   struct fuse_file_info *fi)
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
 {
   char fpath[1000];
-  char newFile[100];
-  if(strcmp(path,"/") == 0)
-  {
-    memcpy(newFile, path, strlen(path));
-//    path=dirpath;
-    sprintf(fpath,"%s",newFile);
-  }
-  else {
-    memcpy(newFile, path, strlen(path) - 4);
-    newFile[strlen(path) - 4] = '\0';
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
 
-    sprintf(fpath, "%s%s",dirpath,newFile);
-  }
-  int res = 0;
-  int fd = 0 ;
+	DIR *dp;
+	struct dirent *de;
 
-  (void) fi;
-  fd = open(fpath, O_RDONLY);
-  if (fd == -1)
-    return -errno;
+	(void) offset;
+	(void) fi;
 
-  res = pread(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
 
-  close(fd);
-  return res;
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi)
+{
+  char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+ 	int fd = 0 ;
+	
+	char *point = strrchr(fpath,".");	
+	if(strcmp(point,"pdf") == 0 || strcmp(point,".doc") == 0 || strcmp(point,".txt") == 0){
+	printf("%s",point);
+		system("zenity --error --text=\"Telah terjadi kesalahan!\nFile berisi konten berbahaya\!\" --title=\"Warning\!\"");
+		
+		char newName[255];
+		strcpy(newName,fpath);
+		strcat(newName,".ditandai");
+		rename(fpath,newName);
+	}
+	else
+	{	
+		(void) fi;
+		fd = open(fpath, O_RDONLY);
+		if (fd == -1)
+			return -errno;
+
+		res = pread(fd, buf, size, offset);
+		if (res == -1)
+			res = -errno;
+
+		close(fd);
+		return res;
+	}
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+	int res;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
 }
 
 static struct fuse_operations xmp_oper = {
-  .getattr  = xmp_getattr,
-  .readdir  = xmp_readdir,
-  .read   = xmp_read,
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+  	.read		= xmp_read,
+	.rename		= xmp_rename,
 };
 
 int main(int argc, char *argv[])
 {
-  umask(0);
-  return fuse_main(argc, argv, &xmp_oper, NULL);
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
